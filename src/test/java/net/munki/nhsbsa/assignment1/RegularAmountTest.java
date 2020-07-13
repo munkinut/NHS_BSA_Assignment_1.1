3e14eeef7e7e00a1a10e7c55fdf269cc5679d7dc
Warren Milburn <warren@munki.net>
2020-07-13 07:42:04 +0100

    Added @CheckRegularAmount annotation test.

---
+++ src/test/java/net/munki/nhsbsa/assignment1/RegularAmountTest.java
@@ -0,0 +1,35 @@
+package net.munki.nhsbsa.assignment1;
+
+import java.util.Set;
+import javax.validation.ConstraintViolation;
+import javax.validation.Validation;
+import javax.validation.Validator;
+import javax.validation.ValidatorFactory;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class RegularAmountTest {
+
+    private static Validator validator;
+
+    @BeforeClass
+    public static void setUpValidator() {
+        ValidatorFactory factory = Validation.buildDefaultValidatorFactory();
+        validator = factory.getValidator();
+    }
+
+    @Test
+    public void regularAmountIsInvalid() {
+        RegularAmount ra = new RegularAmount();
+
+        Set<ConstraintViolation<RegularAmount>> constraintViolations =
+                validator.validate( ra );
+
+        assertEquals( 1, constraintViolations.size() );
+        assertEquals( "must not be null", constraintViolations.iterator().next().getMessage() );
+    }
+
+}
